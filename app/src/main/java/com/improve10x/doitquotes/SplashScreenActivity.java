e44af9e3f8fb91585aa5d0b43390472a4783298f
sunitha <sunithapraveenkumar@gmail.com>
2022-12-29 16:53:23 +0530

    1.Created SplashScreenActivity

---
+++ app/src/main/java/com/improve10x/doitquotes/SplashScreenActivity.java
@@ -0,0 +1,22 @@
+package com.improve10x.doitquotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        handler.postDelayed((() -> {
+            Intent intent = new Intent(this, QuotesActivity.class);
+            startActivity(intent);
+        }), 5000);
+    }
+}
