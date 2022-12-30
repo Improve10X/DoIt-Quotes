788d3f0f1cee98ad00521161caaf43a68417a5c6
K Hareesh <hareesh.komma@improve10x.com>
2022-12-30 23:15:27 +0530

    created a new QuotationsActivity

---
+++ app/src/main/java/com/improve10x/doitquotes/QuotationsActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.doitquotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuotationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quotations);
+    }
+}
