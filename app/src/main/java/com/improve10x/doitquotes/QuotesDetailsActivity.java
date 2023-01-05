6489a3e4c99527c67fbcfaff79bdb7f52fb1b200
sunitha <sunithapraveenkumar@gmail.com>
2023-01-05 19:44:49 +0530

    Modified xml Design

--- app/src/main/java/com/improve10x/doitquotes/QuotesDetailsActivity.java
+++ app/src/main/java/com/improve10x/doitquotes/QuotesDetailsActivity.java
@@ -31,6 +31,8 @@ public class QuotesDetailsActivity extends BaseActivity {
 
     private void showData() {
         Picasso.get().load(quotation.imageUrl).into(binding.imageImg);
+        binding.authorNameTxt.setText(quotation.authorName);
+        binding.quoteTitleTxt.setText(quotation.quoteTitle);
         if (quotation.numberOfLikes !=null && quotation.numberOfLikes.isEmpty() == false) {
             binding.numberOfLikesTxt.setText(quotation.numberOfLikes);
         }
